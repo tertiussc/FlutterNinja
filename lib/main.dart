36c5d9fa50d09d20a377541b5f018cd289615c88
Tertius Scheepers <tertiussc@gmail.com>
2025-01-20 10:36:41 +0200

    Added Home class to enable hot reload

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,24 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+void main() {
+  runApp(MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("My Coffee ID"),
+            backgroundColor: Colors.brown[700],
+            centerTitle: true,
+            titleTextStyle: TextStyle(
+              fontSize: 24,
+            ),
+          ),
+          body: Home())));
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Hello ninjas');
+  }
+}
